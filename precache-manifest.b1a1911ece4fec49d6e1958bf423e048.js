f26edbcc275821defb1cc0ca7e3f22a224f4f05d
sahmed <sahmed@alumni.depaul.edu>
2019-10-22 16:44:15 -0500

    Updates

---
+++ precache-manifest.b1a1911ece4fec49d6e1958bf423e048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7715fe30434502aeb52995c348f72ea4",
+    "url": "/it-daybook-final/index.html"
+  },
+  {
+    "revision": "8da90f61120349161a7e",
+    "url": "/it-daybook-final/static/css/2.8bed955a.chunk.css"
+  },
+  {
+    "revision": "c37c9dde8131792cf559",
+    "url": "/it-daybook-final/static/css/main.5c0360a6.chunk.css"
+  },
+  {
+    "revision": "8da90f61120349161a7e",
+    "url": "/it-daybook-final/static/js/2.4a8806a8.chunk.js"
+  },
+  {
+    "revision": "c37c9dde8131792cf559",
+    "url": "/it-daybook-final/static/js/main.f6069d27.chunk.js"
+  },
+  {
+    "revision": "090a9a81e94577682f5f",
+    "url": "/it-daybook-final/static/js/runtime-main.1bdc7fa2.js"
+  }
+]);
